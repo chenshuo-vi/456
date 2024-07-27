06ddf6b0d5d2a8476ae17d780db062edb472cbd5
chenshuo-vi <84225266+chenshuo-vi@users.noreply.github.com>
2024-07-27 23:24:17 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -39,11 +39,11 @@ sed -i '6s/0/1/' feeds/luci/applications/luci-app-autoreboot/root/etc/config/aut
 #删除upnp dropbear turboacc eqos mwan3helper
 #sed -i '/dropbear/d' include/target.mk
 #sed -i '/ipv6helper\ //d' target/linux/ramips/Makefile
-#sed -i 's/luci-app-upnp\ //g' target/linux/ramips/mt7621/target.mk
-#sed -i 's/luci-app-oaf\ //g' target/linux/ramips/mt7621/target.mk
-#sed -i 's/luci-app-mwan3helper-chinaroute//g' target/linux/ramips/mt7621/target.mk
-#sed -i 's/luci-app-eqos-mtk\ //g' target/linux/ramips/Makefile
-#sed -i 's/luci-app-turboacc-mtk//g' target/linux/ramips/Makefile
+sed -i 's/luci-app-upnp//' target/linux/ramips/mt7621/target.mk
+sed -i 's/luci-app-oaf//' target/linux/ramips/mt7621/target.mk
+sed -i 's/luci-app-mwan3helper-chinaroute//' target/linux/ramips/mt7621/target.mk
+sed -i 's/luci-app-eqos-mtk//' target/linux/ramips/Makefile
+sed -i 's/luci-app-turboacc-mtk//' target/linux/ramips/Makefile
 
 #修复日志错误
 #sed -i '/restart/d' feeds/luci/applications/luci-app-autoreboot/root/etc/init.d/autoreboot
